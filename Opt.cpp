d7eeab80ff5a742808fb9660af37d43fea9388a6
Sergey <.>
2025-03-06 07:43:50 +0300

    Refactor

--- Opt.cpp
+++ Opt.cpp
@@ -5,10 +5,14 @@ Opt::Opt() noexcept : initial_combination(0), combinations(0) {}
 Opt::Opt(const Combination& edges) noexcept : initial_combination(edges), combinations(0) {}
 
 Opt2::Opt2(const Combination& edges) noexcept : Opt(edges) {
+    const size_t A = edges[0].from;
+    const size_t B = edges[0].to;
+    const size_t C = edges[1].from;
+    const size_t D = edges[1].to;
     combinations = {
         {
-            {edges[0].from, edges[1].from},
-            {edges[0].to, edges[1].to}
+            {A, C},
+            {B, D}
         }
     };
 }
@@ -17,17 +21,17 @@ Opt3::Opt3(const Combination& edges) noexcept : Opt(edges) {
     /*
     According to the image from https://github.com/ozanyerli/tsp3opt
     Date of access: 04.03.2025, commit a8d61593a0db4ea9642298678bbc62f1cf6cb9da
-    Letters A, B, C, D, E, F are calculated from top left vertex to top right vertex
+    Letters A, B, C, D, E, F are calculated from top left vertex to top right vertex (counterclockwise)
     Combinations in the picture (according to their case numbers):
     (a) - 0, (b) - 3, (c) - 2, (d) - 1, (e) - 6, (f) - 5, (g) - 4, (h) - 7
     */
-    // Maybe this could be written better using macro or loops
-    auto A = edges[0].from;
-    auto B = edges[0].to;
-    auto C = edges[1].from;
-    auto D = edges[1].to;
-    auto E = edges[2].from;
-    auto F = edges[2].to;
+    // Maybe this could be written better using macro and/or loops
+    const size_t A = edges[0].from;
+    const size_t B = edges[0].to;
+    const size_t C = edges[1].from;
+    const size_t D = edges[1].to;
+    const size_t E = edges[2].from;
+    const size_t F = edges[2].to;
     // Combinations in the order from (b) to (h):
     combinations = {
         {
